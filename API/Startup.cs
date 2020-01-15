56ad437945d52122c11019d2cffe2fe87d7846f5
Willy DONGMO <wdongmo@sarenza.com>
2020-01-15 01:04:02 +0100

    done section3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,11 @@ namespace API
 
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy",policy =>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -43,7 +48,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
